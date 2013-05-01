6aaf51a4c9f4bec6a356ec44393042e1ce952c18
Will Webberley <willwebberley@gmail.com>
2013-05-01 12:54:46 +0100

    Updates for an example workthrough

--- application.py
+++ application.py
@@ -3,28 +3,50 @@ import time, os, json, base64, hmac, sha
 
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route("/account/")
 def home():
-		return render_template('home.html')
+    # Show the account edit HTML page:
+	return render_template('account.html')
+
+@app.route("/submit_form/", methods=["POST"])
+def submit_form():
+    # Collect the data posted from the HTML form in account.html:
+    username = request.form["username"]
+    full_name = request.form["full_name"]
+    avatar_url = request.form["avatar_url"]
+
+    # Provide some procedure for storing the new details
+    update_account(username, full_name, avatar_url)
+    
+    # Redirect to the user's profile page, if appropriate
+    return redirect(url_for('profile'))
 
 @app.route('/sign_s3/')
 def sign_s3():
-    AWS_ACCESS_KEY = os.environ['AWS_ACCESS_KEY_ID']
-    AWS_SECRET_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-    S3_BUCKET = myCoolBucket
+    # Load necessary informatin into the application:
+    AWS_ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    S3_BUCKET = os.environ.get('S3_BUCKET')
     
+    # Collect information on the file from the GET parameters of the request:
     object_name = request.args.get('s3_object_name')
     mime_type = request.args.get('s3_object_type')
  
-    expires = int(time.time()+400)
+    # Set the expiry time of the signature (in seconds) and declare the permissions of the file to be uploaded
+    expires = int(time.time()+10)
     amz_headers = "x-amz-acl:public-read"
  
+    # Generate the PUT request that JavaScript will use:
     put_request = "PUT\n\n%s\n%d\n%s\n/%s/%s" % (mime_type, expires, amz_headers, S3_BUCKET, object_name)
      
-    signature = base64.encodestring(hmac.new(AWS_SECRET, put_request, sha).digest())
+    # Generate the signature with which the request can be signed:
+    signature = base64.encodestring(hmac.new(AWS_SECRET_KEY, put_request, sha).digest())
 
+    # Build the URL of the file in anticipation of its imminent upload:
     url = 'https://%s.s3.amazonaws.com/%s' % (S3_BUCKET, object_name)
     
+    # Return the signed request and the anticipated URL back to the browser in JSON format:
     return json.dumps({
             'signed_request': '%s?AWSAccessKeyId=%s&Expires=%d&Signature=%s' % (url, AWS_ACCESS_KEY, expires, signature),
             'url': url
@@ -34,3 +56,4 @@ def sign_s3():
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+    
