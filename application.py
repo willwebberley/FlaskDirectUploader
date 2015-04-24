f23e2720adb6bc2bf90e49f3967910742f823abb
Will Webberley <willwebberley@gmail.com>
2015-04-24 11:36:55 +0100

    removed all JS dependencies

--- application.py
+++ application.py
@@ -44,31 +44,31 @@ def sign_s3():
     S3_BUCKET = os.environ.get('S3_BUCKET')
 
     # Collect information on the file from the GET parameters of the request:
-    object_name = urllib.quote_plus(request.args.get('s3_object_name'))
-    mime_type = request.args.get('s3_object_type')
+    object_name = urllib.quote_plus(request.args.get('file_name'))
+    mime_type = request.args.get('file_type')
 
     # Set the expiry time of the signature (in seconds) and declare the permissions of the file to be uploaded
-    expires = int(time.time()+10)
+    expires = int(time.time()+60*60*24)
     amz_headers = "x-amz-acl:public-read"
  
     # Generate the PUT request that JavaScript will use:
     put_request = "PUT\n\n%s\n%d\n%s\n/%s/%s" % (mime_type, expires, amz_headers, S3_BUCKET, object_name)
-     
+
     # Generate the signature with which the request can be signed:
-    signature = base64.encodestring(hmac.new(AWS_SECRET_KEY, put_request, sha1).digest())
+    signature = base64.encodestring(hmac.new(AWS_SECRET_KEY, put_request.encode('utf8'), sha1).digest())
     # Remove surrounding whitespace and quote special characters:
     signature = urllib.quote_plus(signature.strip())
 
     # Build the URL of the file in anticipation of its imminent upload:
     url = 'https://%s.s3.amazonaws.com/%s' % (S3_BUCKET, object_name)
 
-    content = json.dumps({
-        'signed_request': '%s?AWSAccessKeyId=%s&Expires=%d&Signature=%s' % (url, AWS_ACCESS_KEY, expires, signature),
-        'url': url
-    })
     
-    # Return the signed request and the anticipated URL back to the browser in JSON format:
-    return Response(content, mimetype='text/plain; charset=x-user-defined')
+    content = json.dumps({
+        'signed_request': '%s?AWSAccessKeyId=%s&Expires=%s&Signature=%s' % (url, AWS_ACCESS_KEY, expires, signature),
+        'url': url,
+    })
+   
+    return content
     
 # Main code
 if __name__ == '__main__':
